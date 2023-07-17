be8e904bddb34aacadd4bb4001d252ccdf21dd5f
EliasNavarroh <eliasnvr@icloud.com>
2023-07-17 09:36:53 -0300

    moving the script of the game to a separate file

---
+++ gamerops.js
@@ -0,0 +1,59 @@
+
+
+function getComputerChoice(){
+    let game = Math.floor(Math.random()*3);
+    if (game == 0){
+        return("Rock").toLowerCase();
+    }else if (game == 1) {
+        return("Paper").toLowerCase();
+    }else {
+        return("Scissors").toLowerCase();
+    }
+}
+ 
+function rpsGame(playerSelection, computerSelection){
+    
+    
+    let game = prompt("Choose: Rock, Paper or Scissors:");
+    playerSelection = game;
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice()
+
+    if (playerSelection == "rock" & computerSelection == "rock"){
+        return alert("Its a Tie")
+    }
+    else if (playerSelection == "rock" & computerSelection == "paper"){
+        return alert("You loose, paper beats rock!")
+    }
+    else if (playerSelection == "rock" & computerSelection == "scissors"){
+        return alert("You Win!, rock beats scissors")
+    }
+    else if (playerSelection == "paper" & computerSelection == "rock"){
+        return alert("You Win!, paper beats rock")
+    }
+    else if (playerSelection == "paper" & computerSelection == "paper"){
+        return  alert("Its a Tie")
+    }
+    else if (playerSelection == "paper" & computerSelection == "scissors"){
+        return alert("You Loose!, scissors beats paper")
+    }
+    else if (playerSelection == "scissors" & computerSelection == "rock"){
+        return alert("You Loose!, rock beats scissors")
+    }
+    else if (playerSelection == "scissors" & computerSelection == "paper"){
+        return alert("You Win!, scissors beats paper")
+    }
+    else {
+        return alert("Its a Tie")
+    }
+}
+
+function game(){
+    let gameCounts = 0;
+    for (gameCounts = 0;gameCounts < 5; gameCounts++){
+        console.log(rpsGame());
+    }
+
+}
+
+console.log(game());
