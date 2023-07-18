198480058b83a729b25ef0279d16c69cd4a2b9df
EliasNavarroh <eliasnvr@icloud.com>
2023-07-18 14:32:39 -0300

    adding the determinewinner to execute after 5 games, also adding the points on the rpsgame in each variable and adding those points in te playerscore and computerscore

--- gamerops.js
+++ gamerops.js
@@ -2,20 +2,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   let rock = document.querySelector(".rock");
   rock.addEventListener('click', function(){
+    console.log("Button pressed")
     rpsGame("rock");
+    determineWinner()
   });
  
   let paper = document.querySelector(".paper");
   paper.addEventListener('click', function(){
+    console.log("Button pressed")
     rpsGame("paper");
+    determineWinner()
   });
 
   let scissors = document.querySelector(".scissors");
   scissors.addEventListener('click', function(){
-  rpsGame("scissors");
+   console.log("Button pressed")
+   rpsGame("scissors");
+   determineWinner()
   });
 
 });
+
+let playerScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice(){
@@ -28,46 +37,96 @@ function getComputerChoice(){
         return("Scissors").toLowerCase();
     }
 }
- 
+
+
 function rpsGame(playerSelection, computerSelection){
     
     computerSelection = getComputerChoice()
 
     if (playerSelection == "rock" & computerSelection == "rock"){
-        return alert("Its a Tie")
+        computerScore = computerScore + 1;
+        playerScore = playerScore+ 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return alert("Computer choose Rock!\n It's a Tie !")
     }
     else if (playerSelection == "rock" & computerSelection == "paper"){
-        return alert("You loose, paper beats rock!")
+        computerScore = computerScore + 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return alert("Computer choose Paper!\n You loose, paper beats rock!")
     }
     else if (playerSelection == "rock" & computerSelection == "scissors"){
-        return alert("You Win!, rock beats scissors")
+        playerScore = playerScore+ 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return alert("Computer choose Scissors!\n You Win!, rock beats scissors")
     }
     else if (playerSelection == "paper" & computerSelection == "rock"){
-        return alert("You Win!, paper beats rock")
+        playerScore = playerScore+ 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return alert("Computer choose Rock !\n You Win!, paper beats rock")
     }
     else if (playerSelection == "paper" & computerSelection == "paper"){
-        return  alert("Its a Tie")
+        computerScore = computerScore + 1;
+        playerScore = playerScore+ 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return  alert("Computer choose Paper!\n It's a Tie")
     }
     else if (playerSelection == "paper" & computerSelection == "scissors"){
-        return alert("You Loose!, scissors beats paper")
+        computerScore = computerScore + 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return alert("Computer choose Scissors!\n You Loose!, scissors beats paper")
     }
     else if (playerSelection == "scissors" & computerSelection == "rock"){
-        return alert("You Loose!, rock beats scissors")
+        computerScore = computerScore + 1;
+        console.log(playerScore);
+        console.log(computerScore);
+        return alert("Computer choose Rock!\n You Loose!, rock beats scissors")
     }
     else if (playerSelection == "scissors" & computerSelection == "paper"){
+        playerScore = playerScore+ 1;
+        console.log(playerScore);
+        console.log(computerScore);
         return alert("You Win!, scissors beats paper")
     }
     else {
+        computerScore = computerScore + 1;
+        playerScore = playerScore+ 1;
+        console.log(playerScore);
+        console.log(computerScore);
         return alert("Its a Tie")
+    }
+    
+    
+}
+
+function determineWinner(){
+    if (playerScore + computerScore == 5){
+        console.log("determineWinner() called after 5 games.");
+        let gameScore = document.querySelector(".game-score");
+        let count = document.createElement("p");
+        let text
+        if (playerScore > computerScore){
+            text = document.createTextNode(`5 Games played:\n you scored: ${playerScore} and the computer: ${computerScore}`);
+            count.appendChild(text);
+        }
+        else if (playerScore < computerScore){
+            text = document.createTextNode(`5 Games played:\n you scored: ${playerScore} and the computer: ${computerScore}`);
+            count.appendChild(text);
+        }
+        else {
+            text = document.createTextNode("It's a Tie!");
+            count.appendChild(text);
+        }
+        count.appendChild(text);
+        gameScore.appendChild(count);
     }
 }
 
-function game(){
-    let gameCounts = 0;
-    for (gameCounts = 0;gameCounts < 5; gameCounts++){
-        console.log(rpsGame());
-    }
-} 
 
-console.log(game());
+
 
